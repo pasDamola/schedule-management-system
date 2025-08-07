487cde67b5789ae15830262d08e5e86049a70a02
pasDamola <oyindadavid18@gmail.com>
2025-08-07 01:28:02 +0100

    remove update appointment trigger

--- backend/internal/database/migrations/001_create_appointments.sql
+++ backend/internal/database/migrations/001_create_appointments.sql
@@ -38,17 +38,3 @@ BEGIN
     );
 END;
 $$ LANGUAGE plpgsql;
-
--- Create trigger to update updated_at timestamp
--- CREATE OR REPLACE FUNCTION update_updated_at_column()
--- RETURNS TRIGGER AS $$
--- BEGIN
---     NEW.updated_at = NOW();
---     RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
-
--- CREATE TRIGGER update_appointments_updated_at
---     BEFORE UPDATE ON appointments
---     FOR EACH ROW
---     EXECUTE FUNCTION update_updated_at_column();
